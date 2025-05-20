5b29b0b455cd1f7ae73269f1219a49279e348c4b
giladOr <gilad.or@uprightpose.com>
2025-05-20 11:25:35 +0300

    removing deprecated token

--- examples/tts-demo/main.js
+++ examples/tts-demo/main.js
@@ -2,7 +2,7 @@ import AiolaTTSClient from "@aiola/web-sdk-tts";
 
 const client = new AiolaTTSClient({
   baseUrl: "https://api.aiola.ai",
-  bearer: "qLDczk3BESZt2vcU1Tqqb1sSJ9DjsKYGygKdRPhLSg0p" //Playground token - Limited usage!!
+  bearer: "<YOUR_BEARER_TOKEN>"
 });
 
 const textInput = document.getElementById("textInput");
